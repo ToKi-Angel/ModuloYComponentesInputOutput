8df7a6441acfda656983dd775e514bc40379bfda
ToKi-Angel <toki.nunez.evangelista@gmail.com>
2023-05-14 20:34:55 -0600

    Initial commit

---
+++ src/app/dbz/pages/main-page/main-page.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Personaje } from '../../interfaces/persona.interface';
+
+@Component({
+  selector: 'app-dbz-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
+})
+export class MainPageComponent {
+  public personajes : Personaje [] = [
+    {nombre : "Goku", poder : 20000}
+  ];
+
+  public obtenNuevoPErsonaje(personaje : Personaje) : void{
+    this.personajes.push(personaje);
+  }
+}
